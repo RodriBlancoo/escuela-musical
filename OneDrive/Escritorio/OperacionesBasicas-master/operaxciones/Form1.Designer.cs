d921dd413151389b8c6710dd45d8c52246d17fb2
Rodrigo <20250111@ricaldone.edu.sv>
2025-06-15 09:12:27 -0600

    fromulario

---
+++ OneDrive/Escritorio/OperacionesBasicas-master/operaxciones/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Operaciones_basicas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnResta = new System.Windows.Forms.Button();
+            this.btnSumar = new System.Windows.Forms.Button();
+            this.lblResultados = new System.Windows.Forms.Label();
+            this.lblN2 = new System.Windows.Forms.Label();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.lblN1 = new System.Windows.Forms.Label();
+            this.lbldatos = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PaleGreen;
+            this.panel1.Controls.Add(this.lbldatos);
+            this.panel1.Controls.Add(this.btnDividir);
+            this.panel1.Controls.Add(this.btnMultiplicar);
+            this.panel1.Controls.Add(this.btnResta);
+            this.panel1.Controls.Add(this.btnSumar);
+            this.panel1.Controls.Add(this.lblResultados);
+            this.panel1.Controls.Add(this.lblN2);
+            this.panel1.Controls.Add(this.txt2);
+            this.panel1.Controls.Add(this.txt1);
+            this.panel1.Controls.Add(this.lblN1);
+            this.panel1.Location = new System.Drawing.Point(122, 54);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(556, 242);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F);
+            this.btnDividir.Location = new System.Drawing.Point(259, 200);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(79, 36);
+            this.btnDividir.TabIndex = 8;
+            this.btnDividir.Text = "Dividir";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F);
+            this.btnMultiplicar.Location = new System.Drawing.Point(381, 200);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(79, 35);
+            this.btnMultiplicar.TabIndex = 7;
+            this.btnMultiplicar.Text = "Multiplicar";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // btnResta
+            // 
+            this.btnResta.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F);
+            this.btnResta.Location = new System.Drawing.Point(134, 201);
+            this.btnResta.Name = "btnResta";
+            this.btnResta.Size = new System.Drawing.Size(80, 35);
+            this.btnResta.TabIndex = 6;
+            this.btnResta.Text = "Resta";
+            this.btnResta.UseVisualStyleBackColor = true;
+            this.btnResta.Click += new System.EventHandler(this.btnResta_Click);
+            // 
+            // btnSumar
+            // 
+            this.btnSumar.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F);
+            this.btnSumar.Location = new System.Drawing.Point(28, 201);
+            this.btnSumar.Name = "btnSumar";
+            this.btnSumar.Size = new System.Drawing.Size(80, 38);
+            this.btnSumar.TabIndex = 5;
+            this.btnSumar.Text = "Sumar";
+            this.btnSumar.UseVisualStyleBackColor = true;
+            this.btnSumar.Click += new System.EventHandler(this.btnSumar_Click);
+            // 
+            // lblResultados
+            // 
+            this.lblResultados.AutoSize = true;
+            this.lblResultados.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F);
+            this.lblResultados.Location = new System.Drawing.Point(413, 68);
+            this.lblResultados.Name = "lblResultados";
+            this.lblResultados.Size = new System.Drawing.Size(72, 19);
+            this.lblResultados.TabIndex = 4;
+            this.lblResultados.Text = "Resultado:";
+            // 
+            // lblN2
+            // 
+            this.lblN2.AutoSize = true;
+            this.lblN2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F);
+            this.lblN2.Location = new System.Drawing.Point(25, 115);
+            this.lblN2.Name = "lblN2";
+            this.lblN2.Size = new System.Drawing.Size(93, 19);
+            this.lblN2.TabIndex = 3;
+            this.lblN2.Text = "Número dos :";
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(28, 150);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(177, 20);
+            this.txt2.TabIndex = 2;
+            this.txt2.TextChanged += new System.EventHandler(this.txt2_TextChanged);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(28, 64);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(177, 20);
+            this.txt1.TabIndex = 1;
+            this.txt1.TextChanged += new System.EventHandler(this.txt1_TextChanged);
+            // 
+            // lblN1
+            // 
+            this.lblN1.AutoSize = true;
+            this.lblN1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblN1.Location = new System.Drawing.Point(25, 32);
+            this.lblN1.Name = "lblN1";
+            this.lblN1.Size = new System.Drawing.Size(91, 19);
+            this.lblN1.TabIndex = 0;
+            this.lblN1.Text = "Número uno:";
+            // 
+            // lbldatos
+            // 
+            this.lbldatos.AutoSize = true;
+            this.lbldatos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbldatos.Location = new System.Drawing.Point(3, 0);
+            this.lbldatos.Name = "lbldatos";
+            this.lbldatos.Size = new System.Drawing.Size(48, 16);
+            this.lbldatos.TabIndex = 1;
+            this.lbldatos.Text = "Datos";
+            this.lbldatos.Click += new System.EventHandler(this.lbldatos_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label lblN1;
+        private System.Windows.Forms.Label lbldatos;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Button btnResta;
+        private System.Windows.Forms.Button btnSumar;
+        private System.Windows.Forms.Label lblResultados;
+        private System.Windows.Forms.Label lblN2;
+        private System.Windows.Forms.TextBox txt2;
+    }
+}
+
---
+++ OneDrive/Escritorio/OperacionesBasicas-master/operaxciones/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Operaciones_basicas
+{
+    public partial class Form1 : Form
+    {
+        //Variable globales
+        double numero_uno, numero_dos, resultado;
+
+        private void txt1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txt1.Text, out numero_uno)&& double.TryParse(txt2.Text, out numero_dos))
+            {
+                resultado = numero_uno+numero_dos;
+                lblResultados.Text = resultado.ToString("N2");
+            }
+            else { lblResultados.Text = "Esto no es posible veriica que ingreses datos valiedos"; }
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txt1.Text, out numero_uno)&&double.TryParse(txt2.Text, out numero_dos))
+            {
+                if (numero_dos != 0)
+                {
+                    resultado = numero_uno / numero_dos;
+                    lblResultados.Text = resultado.ToString("N2");
+                }
+                else
+                {
+                    lblResultados.Text = "Error esto no es posible";
+                }
+            }
+        }
+
+        private void btnResta_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txt1.Text, out numero_uno)&&double.TryParse(txt2.Text, out numero_dos))
+            {
+                if (numero_uno != 0 || numero_dos != 0)
+                {
+                    resultado = numero_uno - numero_dos;
+                    lblResultados.Text = resultado.ToString("N2");
+                }
+                else
+                {
+                    lblResultados.Text = "No se puede realizar esta operación";
+                }
+            }
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            if(double.TryParse(txt1.Text, out numero_uno) && double.TryParse(txt2.Text, out numero_dos))
+            {
+                if (numero_uno >= 0 && numero_dos >= 0)
+                {
+                    resultado = numero_uno * numero_dos;
+                    lblResultados.Text = resultado.ToString("N2");
+                }
+                else
+                {
+                    lblResultados.Text = "No se pudo realizar esta operación";
+                }
+            }
+        }
+
+        private void lbldatos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
